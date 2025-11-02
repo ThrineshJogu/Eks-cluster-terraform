43e18aae389438c21f42b76d739388e04f51c103
ThrineshJogu <joguthrinesh123@gmail.com>
2025-11-02 15:06:12 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "aws_bucket_name" # Replace with your actual S3 bucket name
+    bucket = "thrinesh.jogu.s3.eu" # Replace with your actual S3 bucket name
     key    = "prod/terraform.tfstate"
     region = "eu-north-1"
     use_lockfile= true
